e64f1c4de93150348bcfd2d470a42d31ede2f318
Piyush Kumar <pyuszh@gmail.com>
2026-05-23 14:47:42 +0530

    Added production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials: true
+    baseURL: "https://melodyhub-backend-y195.onrender.com"
+});
+
+api.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default api;

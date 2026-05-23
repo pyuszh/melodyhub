a568017137be4b524e12b56e056acc0d8f2a2035
Piyush Kumar <pyuszh@gmail.com>
2026-05-23 15:02:38 +0530

    final cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.route');
-const musicRoutes = require('./routes/music.route');
+const express = require("express");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth.route");
+const musicRoutes = require("./routes/music.route");
 
 const app = express();
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "*"
 }));
+
 app.use(express.json());
 
-app.use(cookieParser());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/music', musicRoutes);
-
-
-
+app.use("/auth", authRoutes);
+app.use("/music", musicRoutes);
 
 module.exports = app;

aa00b9b6cbabf74e6c19ceb63eab49e3cb195b42
Piyush Kumar <pyuszh@gmail.com>
2026-05-23 14:58:57 +0530

    cors fixed correctly

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,11 +6,12 @@ const musicRoutes = require('./routes/music.route');
 
 
 const app = express();
-app.use(express.json());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+app.use(express.json());
+
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
